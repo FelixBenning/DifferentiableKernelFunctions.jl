812f043374be5d8ab9ff18d438654c552e1cf0c5
Documenter.jl <documenter@juliadocs.github.io>
2023-05-30 07:06:01 +0000

    build based on 107c4e6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DifferentiableKernelFunctions","category":"page"},{"location":"#DifferentiableKernelFunctions","page":"Home","title":"DifferentiableKernelFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DifferentiableKernelFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DifferentiableKernelFunctions]","category":"page"}]
+}
