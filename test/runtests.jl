05a93eb325ba6a562fe2770a35e418dc6b3ff07b
Felix Benning <felix.benning@gmail.com>
2023-05-31 13:01:57 +0200

    make test_selection more flexible

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,24 +2,25 @@ using KernelFunctions: KernelFunctions as KF, MaternKernel, SEKernel
 using DifferentiableKernelFunctions: DifferentiableKernelFunctions as DKF, DiffPt, partial
 using Test
 
+"""
+List of Testfiles without extension. `"$(test).jl"` should be a file for every test in AVAILABLE_TESTS
+"""
 const AVAILABLE_TESTS = [
     "multiOutput",
     "diffKernel",
 ]
 
 function test_selection()
-    group_str = get(ENV, "GROUP", missing)
+    group_str = get(ENV, "TESTSET", missing)
     if ismissing(group_str)
         return AVAILABLE_TESTS
     else
-        groups = split(group_str, ",")
-        surprises = setdiff(groups, AVAILABLE_TESTS)
-        isempty(surprises) || throw(ArgumentError("Test groups $surprises not available"))
-        return groups 
+        return split(group_str, ",")
     end
 end
 
 
+# To select a specific set the "TESTSET" variable (in particular you can also set it to "folder/subfolder/test" not in AVAILABLE_TESTS)
 @testset "DifferentiableKernelFunctions.jl" begin
     for test in test_selection()
         include("$(test).jl")
