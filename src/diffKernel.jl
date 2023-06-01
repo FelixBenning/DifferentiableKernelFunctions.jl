fcccb8d2b7eb406eb081acafa2481bf2d70fc865
Felix Benning <felix.benning@gmail.com>
2023-06-01 09:36:33 +0200

    remove dead code

--- src/diffKernel.jl
+++ src/diffKernel.jl
@@ -2,37 +2,6 @@ import ForwardDiff as FD
 import LinearAlgebra as LA
 using KernelFunctions: SimpleKernel, Kernel
 
-""" 
-	DiffPt(x; partial=())
-
-For a covariance kernel k of GP Z, i.e.
-```julia
-	k(x,y) # = Cov(Z(x), Z(y)),
-```
-a DiffPt allows the differentiation of Z, i.e.	
-```julia
-	k(DiffPt(x, partial=1), y) # = Cov(∂₁Z(x), Z(y))
-```
-for higher order derivatives partial can be any iterable, i.e.
-```julia
-	k(DiffPt(x, partial=(1,2)), y) # = Cov(∂₁∂₂Z(x), Z(y))
-```
-"""
-
-
-# struct DiffPt{Order,KeyT<:IndexType,T}
-#     pos::T # the actual position
-#     partials::NTuple{Order,KeyT}
-# end
-
-# DiffPt(x::T) where {T<:AbstractArray} = DiffPt{0,keytype(T),T}(x, ()::NTuple{0,keytype(T)})
-# DiffPt(x::T) where {T<:Number} = DiffPt{0,Int,T}(x, ()::NTuple{0,Int})
-# DiffPt(x::T, partial::IndexType) where {T} = DiffPt{1,IndexType,T}(x, (partial,))
-# function DiffPt(x::T, partials::NTuple{Order,KeyT}) where {T,Order,KeyT}
-#     return DiffPt{Order,KeyT,T}(x, partials)
-# end
-
-
 """
 	_evaluate(k::T, x::DiffPt{Dim}, y::DiffPt{Dim}) where {Dim, T<:Kernel}
 
